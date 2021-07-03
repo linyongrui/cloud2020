fba475de901fe367a484cca4b36adcdfafa6a17f
TerryLin <lin_yongrui@163.com>
2021-07-04 00:51:00 +0800

    hystrix 降级

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/service/PaymentHystrixService.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/service/PaymentHystrixService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 /**
  */
 @Component
-@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT" /*,fallback = PaymentFallbackService.class*/)
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT" ,fallback = PaymentFallbackService.class)
 public interface PaymentHystrixService
 {
     @GetMapping("/payment/hystrix/ok/{id}")
